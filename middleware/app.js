0c2e4f077fe67aac9c198d1d8305d4fa9f7a0ecc
aminvv <aminviper1378@gmail.com>
2025-04-24 19:33:01 +0330

    middleware camelcase sec:152

--- middleware/app.js
+++ middleware/app.js
@@ -1,29 +1,46 @@
- const express=require("express")
- const app=express()
- const morgan=require("morgan")
+const { default: camelcaseKey } = require("camelcase-keys")
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+//  const camelcaseKey= (...args) =>import("camelcase-keys").then(({default:camelcaseKeys})=>camelcase(...args))  ترفند
 
-app.use(morgan('combined'))
-app.use(morgan('common'))
-app.use(morgan('dev'))
-app.use(morgan('short'))
-app.use(morgan('tiny'))
-app.use(morgan('method: url: :status :res[Content-length]  :response-time ms'))
+// app.use(morgan('combined'))
+// app.use(morgan('common'))
+// app.use(morgan('dev'))
+// app.use(morgan('short'))
+// app.use(morgan('tiny'))
+// app.use(morgan('method: url: :status :res[Content-length]  :response-time ms'))
 
- const auth=(req,res,next)=>{
-    const {username,password}=req.query
-    if(username =="amin" && password==12345){
-        return next()
-    }
-    res.send( "authentication is failed")
- }
+const auth = (req, res, next) => {
+   const { username, password } = req.query
+   if (username == "amin" && password == 12345) {
+      return next()
+   }
+   res.send("authentication is failed")
+}
+
+const camelcase=async(req,res,next)=>{
+req.body=await camelcaseKey(req.body,{deep:true})
+req.query=await camelcaseKey(req.query)
+req.params=await camelcaseKey(req.params)
+
+next()
+}
+
+app.get("/middle", auth, (req, res) => {
+   res.send("user")
+})
+app.use(camelcase)
+app.get("/camel", async (req, res) => {
+   res.send({
+      body:req.body,
+      query:req.query,
+      params:req.params,
+   })
+})
 
 
- app.get("/middle",auth,(req,res)=>{
-    res.send("user")
- })
+app.listen(3000, () => {
+   console.log("http://localhost:3000");
 
-
- app.listen(3000,()=>{
-    console.log("http://localhost:3000");
-    
- })
+})
