3a4cefff0315a48fa8e17fbca63c0a6a3840cb93
aminvv <aminviper1378@gmail.com>
2025-04-24 18:06:36 +0330

    middleware morgan at express sec:151

--- middleware/app.js
+++ middleware/app.js
@@ -1,7 +1,13 @@
  const express=require("express")
  const app=express()
+ const morgan=require("morgan")
 
-
+app.use(morgan('combined'))
+app.use(morgan('common'))
+app.use(morgan('dev'))
+app.use(morgan('short'))
+app.use(morgan('tiny'))
+app.use(morgan('method: url: :status :res[Content-length]  :response-time ms'))
 
  const auth=(req,res,next)=>{
     const {username,password}=req.query
