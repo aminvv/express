14ec411f90773156d9d6746d386981986aad295c
aminvv <aminviper1378@gmail.com>
2025-04-26 14:34:49 +0330

    Error handling in Express sec:155

--- handel-error/index.js
+++ handel-error/index.js
@@ -1,11 +1,23 @@
  const express=require('express')
  const app=express()
  
-
-
  app.get('/',(req,res)=>{
     res.send(req.url)
  })
+ 
+ 
+ app.get('/error/:number', (req, res,next) => {
+try {
+    let number=req.params.number
+
+    if(number ==2)throw ({status:400,message:" number is not equal =2"});
+    if(number ==3)throw ({status:400,message:" number is not equal =3"});
+    if(number ==4)throw ({status:400,message:" number is not equal =4"});
+    res.send("index address")
+} catch (error) {
+    next(error)
+}
+ });
 
 
 
@@ -20,6 +32,23 @@ return res.status(404).json({
     }
 })
 })
+
+
+
+
+
+
+
+app.use((error,req,res,next)=>{
+     return res.json({
+        statusCode:error.status ||500,
+        error:{
+            message:(error.message || "internal server error")
+        }
+     })
+})
+
+
  app.listen(3000,()=>{
     console.log('http://localhost:3000');
  })
