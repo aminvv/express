1eb852f035fd8b17b4680c68000f12d7a6875918
aminvv <aminviper1378@gmail.com>
2025-04-26 16:37:52 +0330

    template engine  pug part one sec:157

---
+++ template-engines/index.js
@@ -0,0 +1,14 @@
+ const {ErrorHandler,NotfoundError}=require('./error-handler')
+ const express=require('express')
+ const app=express()
+ const path=require("path")
+ app.set("view engine","pug")
+ app.set('views',path.join(__dirname,'views'))
+
+
+
+app.use(NotfoundError)
+app.use(ErrorHandler)
+ app.listen(3000,()=>{
+    console.log('http://localhost:3000');
+ })
