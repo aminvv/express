a71658051a90bbc069cc767a26a0c97af5ff3ac8
aminvv <aminviper1378@gmail.com>
2025-04-26 18:06:24 +0330

    send datas and Variable in pug sec:160

--- template-engines/index.js
+++ template-engines/index.js
@@ -1,5 +1,6 @@
  const {ErrorHandler,NotfoundError}=require('./error-handler')
- const express=require('express')
+ const express=require('express');
+const { link } = require('fs');
  const app=express()
  const path=require("path")
  app.use('/static', express.static('public'));
@@ -7,7 +8,10 @@
  app.set('views',path.join(__dirname,'views'))
 
 app.get('/',(req,res)=>{
-    res.render("index")
+    res.render("index",{
+        link:"http://botostart.ir",
+        section:"this is my section"
+    })
 })
 
 app.use(NotfoundError)
