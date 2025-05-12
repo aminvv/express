8ea328b74ff36d5b7a50f5e86b3a3f59586cf834
aminvv <aminviper1378@gmail.com>
2025-05-12 11:38:09 +0330

    File format validation in multer sec:198

--- file-upload/multer/middleware/multer.js
+++ file-upload/multer/middleware/multer.js
@@ -1,26 +1,34 @@
- const  multer=require('multer')
- const  path=require('path')
- const  fs=require('fs')
- 
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
- const storage= multer.diskStorage({
-    destination:function(req,file,callB){
-        fs.mkdirSync('public/uploads',{recursive:true})
-        callB(null,'public/uploads')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callB) {
+        fs.mkdirSync('public/uploads', { recursive: true })
+        callB(null, 'public/uploads')
     },
-    filename: function(req,file,callB){
+    filename: function (req, file, callB) {
 
-        const ext=path.extname(file.originalname)
-        const witeListFormat=['.jpg','jpeg','.png','webp' ]
-        if(witeListFormat.includes(ext)){
-                    const filename=Date.now() +ext
-        callB(null,filename)
-        }else{
+        const ext = path.extname(file.originalname)
+        const witeListFormat = ['.jpg', 'jpeg', '.png', 'webp']
+        if (witeListFormat.includes(ext)) {
+            const filename = Date.now() + ext
+            callB(null, filename)
+        } else {
             callB(new Error(" only .png .jpeg .jpg .webp format allowed"))
         }
-    }
- })
+    },
 
- const uploadFile=multer({storage})
+})
 
- module.exports=uploadFile
+
+const _150KB=150000
+const uploadFile = multer({
+    storage,
+    limits: {
+        fileSize:_150KB
+}
+})
+
+module.exports = uploadFile
