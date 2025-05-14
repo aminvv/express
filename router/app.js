5a425ad998b151784c54908dd7d57267aadf4104
aminvv <aminviper1378@gmail.com>
2025-05-14 14:46:23 +0330

    routing  and router method in express sec:205

---
+++ router/app.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const { allRouter } = require('./router');
+const app=express()
+
+app.use(allRouter)
+
+app.listen(3000,()=>{
+    console.log("http://localhost:3000  run server on port 3000" );
+})
---
+++ router/router/index.js
@@ -0,0 +1,12 @@
+const{Router}=require('express')
+const {userRouter} = require('./user.router')
+const router=Router()
+
+
+
+router.use('/user/',userRouter)
+
+
+module.exports={
+    allRouter:router
+}
