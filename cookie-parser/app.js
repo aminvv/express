581170a8aea37af7285b103af761b940f78195d2
aminvv <aminviper1378@gmail.com>
2025-05-15 11:17:55 +0330

    get cookie in express sec:211

--- cookie-parser/app.js
+++ cookie-parser/app.js
@@ -12,6 +12,9 @@ app.get('/set-cookie',(req,res)=>{
     res.cookie('cookie--name','cookie--value')
     res.send('cookie set successfully')
 })
+app.get('/get-cookie',(req,res)=>{
+    res.send(req.cookies)
+})
 
 app.listen(3000,()=>{
     console.log('http://localhost:3000');
