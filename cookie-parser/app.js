51d8f9db548f6bcbd6a97fceafa5e5ac2c741134
aminvv <aminviper1378@gmail.com>
2025-05-15 18:44:41 +0330

    clear cookie in express sec:214-215

--- cookie-parser/app.js
+++ cookie-parser/app.js
@@ -9,19 +9,25 @@ app.use(cookieParser('jsdhfhkjdgsfkhjghjgajfhkljadhsjhfjhckjghadfjkshgjklads'))
 
 
 app.get('/set-cookie',(req,res)=>{
-    res.cookie('cookie--nodejs','cookie--value----nodejs',{
+    res.cookie('cookie--name','cookie--value----nodejs',{
         expires: new Date(new Date().getTime()+ 5000)
     })
     res.cookie('cookie--python','cookie--value----python',{
         maxAge:5000,
         httpOnly:true,
         signed:true,
-        secure:true
+        secure:true,
+        sameSite:'lax',// lax -strict- none
     })
     res.send('cookie set successfully')
 })
 app.get('/get-cookie',(req,res)=>{
-    res.send(req.cookies)
+    res.send(req.cookies.name)
+})
+
+app.get('/clear-cookie',(req,res)=>{
+    res.clearCookie("cookie--name")
+    res.send('clear cookie successfully')
 })
 
 app.listen(3000,()=>{
