92dbbe6f05c03f970b355108f94c941fa2640f2f
aminvv <aminviper1378@gmail.com>
2025-05-15 11:30:20 +0330

    option cookie (maxAge -- expirres) sec:212

--- cookie-parser/app.js
+++ cookie-parser/app.js
@@ -9,7 +9,12 @@ app.use(cookieParser())
 
 
 app.get('/set-cookie',(req,res)=>{
-    res.cookie('cookie--name','cookie--value')
+    res.cookie('cookie--nodejs','cookie--value----nodejs',{
+        expires: new Date(new Date().getTime()+ 5000)
+    })
+    res.cookie('cookie--python','cookie--value----python',{
+        maxAge:5000,
+    })
     res.send('cookie set successfully')
 })
 app.get('/get-cookie',(req,res)=>{
