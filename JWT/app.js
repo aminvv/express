dabc028b5a22b68cdb2ed3444870ad0842c9c620
aminvv <aminviper1378@gmail.com>
2025-05-17 17:04:29 +0330

    ((verify)) and ((decoded)) token sec:229

--- JWT/app.js
+++ JWT/app.js
@@ -6,8 +6,7 @@ const privateKey=fs.readFileSync('./privateKey.key')
 const tokenHS512 = jwt.sign({ id: 'sayMyname', email: 'amin@gmail.com' },
     "dskfhdskljahg;adlshfdslijuhksdf;lkjkljDSSFL:k",
     {
-        expiresIn: '1h',
-        algorithm: 'HS512'
+        expiresIn: '2s',
     }
 )
 console.log('TOKEN HS512=========>>>>>>',tokenHS512);
@@ -23,4 +22,30 @@ const tokenRS512 = jwt.sign({ id: 'sayMyname', email: 'amin@gmail.com' },
     }
 )
 
-console.log('TOKEN RS512=========>>>>>>',tokenRS512);
+console.log('TOKEN RS512=========>>>>>>',tokenRS512 );
+
+
+
+
+// -----------------------------------------------------------------------------------------------------------------verify
+// Important------>>>>>>*****    expires -- secret
+
+setTimeout(()=>{
+try {
+        const tokenVerify = jwt.verify(tokenHS512,"dskfhdskljahg;adlshfdslijuhksdf;lkjkljDSSFL:k")
+
+console.log('TOKEN verify =========>>>>>>',tokenVerify );
+} catch (error) {
+    console.log(error);
+}
+},3000)
+ 
+
+// ======= decode
+// not Important ----->>>>>>******** expires --- secret 
+setTimeout(()=>{
+    const tokenDecode = jwt.decode(tokenHS512,)
+
+console.log('TOKEN decode =========>>>>>>',tokenDecode );
+},3000)
+ 
