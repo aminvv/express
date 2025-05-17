98d75ea927a7dd4e9ac579519b5134785e433227
aminvv <aminviper1378@gmail.com>
2025-05-17 11:42:42 +0330

    method hashed password with (( createHash -- createHmac )) sec:220-221

--- hashing/app.js
+++ hashing/app.js
@@ -23,3 +23,26 @@ function verifyHashPassword(password,hashPassword) {
 const hashed=hashPassword('123456')
 const result=verifyHashPassword('123456',hashed)
 console.log(result);
+
+
+
+
+
+
+//  create hashed ------  2
+
+
+
+const hashed_two=crypto.createHash('sha512',{encoding:"utf-8"}).update('123456').digest('hex')
+console.log('hashed :2',hashed_two);
+
+
+
+//  create hashed ------  3
+
+
+// const secret="sdjfhjhadsghgfjkljadskljhfgjkadshjfhjadsh"
+// or
+const secret=crypto.randomBytes(16).toString('hex')
+const hashed_three=crypto.createHmac('sha512',secret).update('123456').digest('hex')
+console.log('hashed :3',hashed_three); 
