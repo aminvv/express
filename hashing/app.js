76e84f532d03415b445a7c9c5bd65e2ac865d4b1
aminvv <aminviper1378@gmail.com>
2025-05-17 11:09:15 +0330

    validation hashed password in package crypto sec:219

--- hashing/app.js
+++ hashing/app.js
@@ -7,7 +7,19 @@ const crypto = require('crypto')
 
 function hashPassword(password) {
     const salt = crypto.randomBytes(16).toString('hex')
-    const hashed = crypto.pbkdf2Sync('123456', salt, 1000, 64, 'sha512').toString('hex')
+    const hashed = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
     const newHashed = `$2s.${salt}.${hashed}`
     return newHashed
 }
+
+
+function verifyHashPassword(password,hashPassword) {
+    const salt = hashPassword.split(".")?.[1]
+    const hashed = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
+    const newHashed = `$2s.${salt}.${hashed}`
+    return (hashPassword ==newHashed)
+}
+
+const hashed=hashPassword('123456')
+const result=verifyHashPassword('123456',hashed)
+console.log(result);
